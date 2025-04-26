33b169092e7f16b50bc6e01fd1aa71e1f075ed79
Yahya <yahyamdaud21@gmail.com>
2025-04-26 16:24:48 -0700

    Post request uploading a new event!

--- my-app/app/api/all-events/route.ts
+++ my-app/app/api/all-events/route.ts
@@ -1,5 +1,7 @@
 import { getAllEvents } from "@/lib/actions";
 import { NextRequest, NextResponse } from "next/server";
+import EventModel from "@/lib/models/event";
+import { createEvent } from "@/lib/actions";
 
 type Event = {
   id: string;
@@ -11,6 +13,7 @@ type Event = {
 
 export async function GET(request: NextRequest) {
   try {
+    //await connect();
     const { searchParams } = new URL(request.url);
 
     const category = searchParams.get('category');
@@ -37,5 +40,20 @@ export async function GET(request: NextRequest) {
   } catch (error) {
     console.log(error);
     return NextResponse.json({ error: "Error fetching events" }, { status: 500 });
+  }
+}
+
+export async function POST(request: NextRequest) {
+  try {
+    //await connect();
+
+    const body = await request.json();
+
+    const savedEvent = createEvent(body);
+
+    return NextResponse.json(savedEvent, { status: 201 }); 
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: "Error creating event" }, { status: 500 });
   }
 }
--- my-app/lib/models/event.ts
+++ my-app/lib/models/event.ts
@@ -3,14 +3,14 @@ import { Schema, model, models } from "mongoose";
 const EventSchema = new Schema({
     title: { type: String, required: true },
     body: { type: String, required: true },
-    organizer: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    //organizer: { type: Schema.Types.ObjectId, ref: "User", required: false },
     date: { type: Date, required: true },
     location: { type: String, required: false },
     thumbnail: { type: String, required: false },
     maxAttendees: { type: Number, required: false },
     contact: { type: String, required: false },
     price: { type: Number, required: false },
-    isPublic: { type: Boolean, required: true },
+    isPublic: { type: Boolean, required: false },
     tags: { type: [String], required: false }
 
 });
